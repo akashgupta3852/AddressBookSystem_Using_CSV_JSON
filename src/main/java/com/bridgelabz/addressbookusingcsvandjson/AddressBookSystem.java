4560d80329448ffd5d3400a63a53c68f284ced46
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-19 01:06:59 +0530

    [Akash]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/addressbookusingcsvandjson/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookusingcsvandjson;
+
+public class AddressBookSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
